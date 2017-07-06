37c25390f75c0f234d264456c0df6bb6eaf34439
Stefan Froemken <froemken@gmail.com>
2017-07-06 23:52:52 +0200

    Add TYPO3 8 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-9.99.99',
+            'typo3' => '7.6.0-8.99.99',
         ),
         'conflicts' => array(
         ),
